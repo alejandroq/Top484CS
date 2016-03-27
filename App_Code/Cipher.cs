ed5996152d25f696c38e5737c371dd2e2ec4ff98
ledesmac <alex.ledesma7@gmail.com>
2016-03-27 19:39:56 -0400

    Updated Variables

--- App_Code/Cipher.cs
+++ App_Code/Cipher.cs
@@ -1,26 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 /***********************************
- * Class: Parent
+ * Class: Cipher
  * Author:Alex Ledesma
- * Version:1
- * Parent is a class that Inherits User and will be used to control
- * parent records and actions
+ * Version: 1
+ * Cipher is a class that Inherits User and will be used to control
+ * cipher records and actions
  **********************************/
 public class Cipher : User
 {
-    private ArrayList students;
+    protected bool paid;
     /******************************************
      * Explicit Constructor
-     * Sets applicant permission to lowest possible
-     * until user is approved
      * @param emailAddress user's email address doubles as login
      * @param fName, String user's first name
      * @param lName, String user's last name
-     * @param MI, String user's middle initial 
+     * @param userType, String user's account type 
      * @param address, String user's home address
      * @param city, String user's home city
      * @param state, String user's home state
@@ -30,35 +29,55 @@ public class Cipher : User
      * @param gender, char identifying users gender
      * @param homePhone, user's home phone number
      * @param cellNum, user's cell phone number
+     * @param size, user's shirt size
+     * @param race, user's race
+     * @param paid, boolean identifying if user has paid membership fee or not
      *****************************************/
-    public Cipher(String emailAddress, String fName, String lName, String MI,
+    public Cipher(String emailAddress, String fName, String lName, String userType,
     String DOB, String address, String city, String state, int zip, String pass,
-    char gender, String homePhone, String cellNum, String ward, String dcOne)
-        : base(emailAddress, fName, lName, MI,
-            DOB, address, city, state, zip, pass, gender, homePhone, cellNum)
-    {
+    char gender, String homePhone, String cellNum, String size, String race, bool paid)
+        : base(emailAddress, fName, lName, userType, DOB, address, city, state, zip, pass, gender, homePhone, cellNum, size, race)
+	{
         base.setPermission(2);
-        setWard(ward);
-        setDCOneCardNum(dcOne);
-    }
+        setPaid(paid);
+        setActivated(true);
+	}
 
-    public Cipher(Applicant app, String ward, String dcOne)
+    public Cipher(Applicant app, bool paid)
     {
         setEmail(app.getEmail());
         setFName(app.getFName());
         setLName(app.getLName());
-        setMI(app.getMI());
+        setUserType(app.getUserType());
         setDOB(app.getDOB());
         setAddress(app.getAddress());
         setCity(app.getCity());
         setState(app.getState());
         setZip(app.getZip());
-        setPass(app.getPass);
-        base.setPermission(4);
-        setWard(ward);
-        setDCOneCardNum(dcOne);
+        setPass(app.getPass());
+        base.setPermission(2);
         setGender(app.getGender());
         setHomePhone(app.getHomePhone());
         setCellPhone(app.getCellPhone());
+        setPaid(paid);
+        setRace(app.getRace());
+        setShirtSize(app.getShirtSize());
+        setActivated(true);
     }
+
+   /*******************************************
+   * Accessor Method
+   * @return user's paid membership fee or not
+   ******************************************/
+    public bool getPaid() { return this.paid; }
+    
+    /*******************************************
+    * Mutator Method
+    * @param bool saying whether or not user has paid membership fee
+    ******************************************/
+    protected void setPaid(bool paid)
+    {
+        this.paid = paid;
+    }
+
 }
