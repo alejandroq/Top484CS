b0d193849301f2579f51cc38901a5e527f319c74
alejandroq <quesadar@dukes.jmu.edu>
2016-03-28 23:51:09 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 $("#menu").menu();
+
+$('#stackMenu').on('click', function() {
+	if ($('nav').is(':visible')){
+		$('#stackMenu').css('background-color', '#cccccc');
+	} else {
+		$('#stackMenu').css('background-color', '#ffffff');
+	}
+	$('nav').toggle('blind',150);
+});
