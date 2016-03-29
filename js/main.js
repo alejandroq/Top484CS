9ef0e0f02e9ef00ab7dd0dd0e3ac475d9871d3ba
alejandroq <quesadar@dukes.jmu.edu>
2016-03-29 14:39:47 -0400

    Master Page

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $("#menu").menu();
 
 $('#stackMenu').on('click', function() {
 	if ($('nav').is(':visible')){
-		$('#stackMenu').css('background-color', '#cccccc');
+		$('#stackMenu').css('background-color', '#B94657'); //#cccccc
 	} else {
-		$('#stackMenu').css('background-color', '#ffffff');
+		$('#stackMenu').css('background-color', '#ffffff'); //#ffffff
 	}
 	$('nav').toggle('blind',150);
 });
