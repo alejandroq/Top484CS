da21d2a3085f11a18aa5af66d80c915c2d362956
alejandroq <quesadar@dukes.jmu.edu>
2016-03-29 16:55:17 -0400

    Stuff

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ $(".menu").menu();
 
 $('#stackMenu').on('click', function() {
 	if ($('nav').is(':visible')){
-		$('#stackMenu').css('background-color', '#B94657'); //#cccccc
+		$('#stackMenu').css('background-color', '#cccccc'); //#cccccc #B94657
 	} else {
 		$('#stackMenu').css('background-color', '#ffffff'); //#ffffff
 	}
 	$('nav').toggle('blind',150);
+});
+
+$('#notificationMenu').on('click', function() {
+	if ($('#ui-data-menu').is(':visible')){
+		$('#notificationMenu').css('background-color', '#cccccc'); //#cccccc #B94657
+	} else {
+		$('#notificationMenu').css('background-color', '#ffffff'); //#ffffff
+	}
+	$('#ui-data-menu').toggle('blind',150);
 });
