bb2838d8db21c84cf5209ff762dec50a5cd5368c
Hahnvesi <Hahnvesi@dukes.jmu.edu>
2016-03-30 15:18:20 -0400

    Added files via upload
    backend homepage

---
+++ Instructor.StudentEvaluationHomePage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Instructor_StudentEvaluationHomePage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSubmitEvaluation_Click(object sender, EventArgs e)
+    {
+
+    }
+}
