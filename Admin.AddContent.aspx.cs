2959b9376b0d3ef7314df6a7c6f6a24da18b6209
Hahnvesi <Hahnvesi@dukes.jmu.edu>
2016-03-30 14:09:18 -0400

    Added files via upload
    master page additions

--- Admin.AddContent.aspx.cs
+++ Admin.AddContent.aspx.cs
@@ -14,20 +14,20 @@ public partial class Admin_AddContent : System.Web.UI.Page
     protected void btnUpload_Click(object sender, EventArgs e)
     {
         if (flAdminAddContent.HasFile)
-            {
-                string filename = System.IO.Path.GetFileName(flAdminAddContent.FileName);
-                flAdminAddContent.SaveAs(@"C:\Users\lee2rm\Desktop\Portfolios\" + filename); // used this filepath to save on my desktop
-                /*Uploaded file path*/
-                /*******************************************/
-                /*Code to save the file path into data base??*/
-                /*******************************************/
-                Response.Write("File uploaded successfully.");
-                // need to tie the file uploaded to the selected student's folder
-                // need to add event handler for publishing file to community wall if user selects check box
-            }
-            else
-            {
-                Response.Write("Error: Please select a file");
-            }
+        {
+            string filename = System.IO.Path.GetFileName(flAdminAddContent.FileName);
+            flAdminAddContent.SaveAs(@"C:\Users\lee2rm\Desktop\Portfolios\" + filename); // used this filepath to save on my desktop
+            /*Uploaded file path*/
+            /*******************************************/
+            /*Code to save the file path into data base??*/
+            /*******************************************/
+            Response.Write("File uploaded successfully.");
+            // need to tie the file uploaded to the selected student's folder
+            // need to add event handler for publishing file to community wall if user selects check box
         }
+        else
+        {
+            Response.Write("Error: Please select a file");
+        }
+    }
 }
