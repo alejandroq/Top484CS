4f52b4bfe0d470bd5f0a060b2afbda553d976baa
ropperdx <ropperdx@dukes.jmu.edu>
2016-04-03 19:53:22 -0400

    Added files via upload
    parameteried and extended for other attributes

--- Admin.EditUser.aspx
+++ Admin.EditUser.aspx
@@ -14,6 +14,17 @@
         <br />
         <asp:Label ID="lblfName" runat="server" Text="First Name: "><asp:TextBox ID="txtfName" runat="server"></asp:TextBox></asp:Label><br />
         <asp:Label ID="lbllName" runat="server" Text="Last Name: "><asp:TextBox ID="txtlName" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblDOB" runat="server" Text="Date of Birth: "><asp:TextBox ID="txtDOB" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblCellPhone" runat="server" Text="Cell Phone: "><asp:TextBox ID="txtCellPhone" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblHomePhone" runat="server" Text="Home Phone: "><asp:TextBox ID="txtHomePhone" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblHomeAddress" runat="server" Text="Home Address: "><asp:TextBox ID="txtHomeAddress" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblCity" runat="server" Text="City: "><asp:TextBox ID="txtCity" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblState" runat="server" Text="State: "><asp:TextBox ID="txtState" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblZip" runat="server" Text="Zip: "><asp:TextBox ID="txtZip" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblGender" runat="server" Text="Gender: "><asp:TextBox ID="txtGender" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblRace" runat="server" Text="Race: "><asp:TextBox ID="txtRace" runat="server"></asp:TextBox></asp:Label><br />
+        <asp:Label ID="lblShirt" runat="server" Text="Shirt Size: "><asp:TextBox ID="txtShirt" runat="server"></asp:TextBox></asp:Label><br />
+                
         <asp:Button ID="btnEditUser" runat="server" Text="Update User Information" OnClick="btnEditUser_Click" />
     </div>
     </form>
--- Admin.EditUser.aspx.cs
+++ Admin.EditUser.aspx.cs
@@ -23,41 +23,65 @@ public partial class Admin_EditUser : System.Web.UI.Page
     {
         //change back
         // Session variable containing the Email address of the selected user clicked on from ManageAccounts page is passed in
-        string userID = Session["userID"].ToString();
-        //String userID = "testAdmin@WBL.org";
+        //string userID = Session["userID"].ToString();
+        String userID = "testAdmin@WBL.org";
 
         //change back
-        System.Diagnostics.Debug.WriteLine(Session["userID"].ToString());
+        //System.Diagnostics.Debug.WriteLine(Session["userID"].ToString());
         SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
         connection.Open();
-        string cmdText = "select FirstName, LastName, EmailAddress from dbo.GeneralUser where EmailAddress='" + userID + "'";
+        string cmdText = "select FirstName, LastName, EmailAddress, CellPhone, HomePhone, DOB, ShirtSize, Gender, Race, HomeAddress, City, State, Zip from dbo.GeneralUser where EmailAddress='" + userID + "'";
         System.Diagnostics.Debug.WriteLine(cmdText);
         SqlCommand cmd = new SqlCommand(cmdText, connection);
         cmd.ExecuteNonQuery();
         SqlDataAdapter adp = new SqlDataAdapter(cmd); // read in data from query results
         DataTable dt = new DataTable(); // create data table for sql query
         adp.Fill(dt);
-        string fullName = dt.Rows[0][0].ToString();
+        //string fullName = dt.Rows[0][0].ToString();
         //NEED TO BREAK THIS STRING INTO FIRST AND LAST NAME
-        //txtfName.Text = dt.Rows[0][0].ToString();
-        //txtlName.Text = dt.Rows[0][1].ToString();
+        txtfName.Text = dt.Rows[0][0].ToString();
+        txtlName.Text = dt.Rows[0][1].ToString();
         txtEmail.Text = dt.Rows[0][2].ToString();
         txtEmail.ReadOnly = true; // Set to un-editable since this will violate FK, Admin shouldnt be able to change this anyways?
-        // add the rest of the fields here in the same methodology
+
+        txtCellPhone.Text = dt.Rows[0][3].ToString();
+        txtHomePhone.Text = dt.Rows[0][4].ToString();
+        txtDOB.Text = dt.Rows[0][5].ToString();
+        txtShirt.Text = dt.Rows[0][6].ToString();
+        txtGender.Text = dt.Rows[0][7].ToString();
+        txtRace.Text = dt.Rows[0][8].ToString();
+        txtHomeAddress.Text = dt.Rows[0][9].ToString();
+        txtCity.Text = dt.Rows[0][10].ToString();
+        txtState.Text = dt.Rows[0][11].ToString();
+        txtZip.Text = dt.Rows[0][12].ToString();
+
     }
     protected void btnEditUser_Click(object sender, EventArgs e)
     {
         ViewState["fName"] = txtfName.Text;
         ViewState["lName"] = txtlName.Text;
-        string email = txtEmail.Text;
+        //string email = txtEmail.Text;
+        //string email = Session["userID"].ToString();
+        String email = "testAdmin@WBL.org";
         SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ConnectionString);
         connection.Open();
-        string cmdText = "update dbo.GeneralUser set FirstName = @FirstName, LastName = @LastName where EmailAddress = @EmailAddress";
+        string cmdText = "update dbo.GeneralUser set FirstName = @FirstName, LastName = @LastName, CellPhone = @CellPhone, HomePhone = @HomePhone, DOB = @DOB, ShirtSize = @ShirtSize, Gender = @Gender, Race = @Race, HomeAddress = @HomeAddress, City = @City, State = @State, Zip = @Zip where EmailAddress = @EmailAddress";
         System.Diagnostics.Debug.WriteLine(cmdText);
         SqlCommand cmd = new SqlCommand(cmdText, connection);
         cmd.Parameters.AddWithValue("@FirstName", ViewState["fName"]);
         cmd.Parameters.AddWithValue("@LastName", ViewState["lName"]);
-        cmd.Parameters.AddWithValue("@EmailAddress", txtEmail.Text);
+        cmd.Parameters.AddWithValue("@EmailAddress", email);
+        cmd.Parameters.AddWithValue("@CellPhone", txtCellPhone.Text);
+        cmd.Parameters.AddWithValue("@HomePhone", txtHomePhone.Text);
+        cmd.Parameters.AddWithValue("@DOB", txtDOB.Text);
+        cmd.Parameters.AddWithValue("@ShirtSize", txtShirt.Text);
+        cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+        cmd.Parameters.AddWithValue("@Race", txtRace.Text);
+        cmd.Parameters.AddWithValue("@HomeAddress", txtHomeAddress.Text);
+        cmd.Parameters.AddWithValue("@City", txtCity.Text);
+        cmd.Parameters.AddWithValue("@State", txtState.Text);
+        cmd.Parameters.AddWithValue("@Zip", txtZip.Text);
+
         cmd.ExecuteNonQuery();
         Response.Redirect("Admin.ManageAccounts.aspx", false);
     }
