f54ef009bda126301e4d9cbf076a31c33c17124e
Hahnvesi <Hahnvesi@dukes.jmu.edu>
2016-04-03 17:46:14 -0400

    Added files via upload
    new page

---
+++ MyAccount.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MyAccount : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        int userType = Int32.Parse(Session["permission"].ToString());
+
+        switch (userType)
+        {
+            case 1:
+                // display cipher menu
+                adminPanel1.Style["display"] = "none";
+                adminFooter.Style["display"] = "none";
+
+                parentPanel1.Style["display"] = "none";
+                parentFooter.Style["display"] = "none";
+
+                studentPanel1.Style["display"] = "none";
+                studentFooter.Style["display"] = "none";
+
+                instructorPanel1.Style["display"] = "none";
+                instructorFooter.Style["display"] = "none";
+
+                btnViewProfile.Style["display"] = "none";
+
+                break;
+            case 2:
+                // display parent menu
+                adminPanel1.Style["display"] = "none";
+                adminFooter.Style["display"] = "none";
+
+                cipherPanel1.Style["display"] = "none";
+                cipherFooter.Style["display"] = "none";
+
+                studentPanel1.Style["display"] = "none";
+                studentFooter.Style["display"] = "none";
+
+                instructorPanel1.Style["display"] = "none";
+                instructorFooter.Style["display"] = "none";
+
+                btnViewProfile.Style["display"] = "none";
+
+                break;
+            case 3:
+                // display student menu
+                adminPanel1.Style["display"] = "none";
+                adminFooter.Style["display"] = "none";
+
+                cipherPanel1.Style["display"] = "none";
+                cipherFooter.Style["display"] = "none";
+
+                parentPanel1.Style["display"] = "none";
+                parentFooter.Style["display"] = "none";
+
+                instructorPanel1.Style["display"] = "none";
+                instructorFooter.Style["display"] = "none";
+
+                break;
+            case 4:
+                // display instructor menu
+                adminPanel1.Style["display"] = "none";
+                adminFooter.Style["display"] = "none";
+
+                cipherPanel1.Style["display"] = "none";
+                cipherFooter.Style["display"] = "none";
+
+                parentPanel1.Style["display"] = "none";
+                parentFooter.Style["display"] = "none";
+
+                studentPanel1.Style["display"] = "none";
+                studentFooter.Style["display"] = "none";
+
+                btnViewProfile.Style["display"] = "none";
+
+                break;
+            case 5:
+                // display admin menu
+                cipherPanel1.Style["display"] = "none";
+                cipherFooter.Style["display"] = "none";
+
+                parentPanel1.Style["display"] = "none";
+                parentFooter.Style["display"] = "none";
+
+                studentPanel1.Style["display"] = "none";
+                studentFooter.Style["display"] = "none";
+
+                instructorPanel1.Style["display"] = "none";
+                instructorFooter.Style["display"] = "none";
+
+                btnViewProfile.Style["display"] = "none";
+                break;
+            default:
+                // ?? display error?
+                break;
+        }
+
+    }
+    protected void btnChangePassword_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ChangePassword.aspx");
+    }
+    protected void btnViewProfile_Click(object sender, EventArgs e)
+    {
+        // link to student's profile page using session variable
+    }
+    protected void btnEditInfo_Click(object sender, EventArgs e)
+    {
+
+    }
+}
