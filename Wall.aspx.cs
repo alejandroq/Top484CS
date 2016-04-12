d3d12239cff1943d73720c9fde9790bcfa864c41
Hahnvesi <Hahnvesi@dukes.jmu.edu>
2016-04-12 19:28:46 -0400

    sierras integration

--- Wall.aspx.cs
+++ Wall.aspx.cs
@@ -21,9 +21,8 @@ public partial class Wall : System.Web.UI.Page
             case 1:
                 // display cipher menu
                 adminPanel1.Style["display"] = "none";
-                
+
                 parentPanel1.Style["display"] = "none";
-           
 
                 studentPanel1.Style["display"] = "none";
 
@@ -35,37 +34,43 @@ public partial class Wall : System.Web.UI.Page
                 adminPanel1.Style["display"] = "none";
 
                 cipherPanel1.Style["display"] = "none";
-
                 studentPanel1.Style["display"] = "none";
-
                 instructorPanel1.Style["display"] = "none";
-
                 break;
             case 3:
                 // display student menu
                 adminPanel1.Style["display"] = "none";
+                
 
                 cipherPanel1.Style["display"] = "none";
+                
 
                 parentPanel1.Style["display"] = "none";
+                
 
                 instructorPanel1.Style["display"] = "none";
+                
 
                 break;
             case 4:
                 // display instructor menu
                 adminPanel1.Style["display"] = "none";
+                
 
                 cipherPanel1.Style["display"] = "none";
+                
 
                 parentPanel1.Style["display"] = "none";
+                
 
                 studentPanel1.Style["display"] = "none";
+                
 
                 break;
             case 5:
                 // display admin menu
                 cipherPanel1.Style["display"] = "none";
+                
 
                 parentPanel1.Style["display"] = "none";
 
